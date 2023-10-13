3a9d440eaf026f9dd228f682d7f3ca2c7a9c8e7b
akadir8 <bickiabdulkadir12@gmail.com>
2023-10-13 13:49:33 +0300

    Update

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,11 +2,16 @@ import Link from "next/link";
 import React from "react";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
-import { Article } from "./article";
+import data from "../components/data.json";
+import { Data } from "@/util/dataTypes"
 
 export const revalidate = 60;
+
 export default async function ProjectsPage() {
-  return (
+	const projects: Data = data; // data nesnesini Data tipine atayın
+  
+  
+	return (
     <div className="relative pb-16">
       <Navigation />
       <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
@@ -20,19 +25,26 @@ export default async function ProjectsPage() {
         </div>
         <div className="w-full h-px bg-zinc-800" />
 
-        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
-          <Card>
-            <Link href={`/projects`}>
-              <iframe
-                src="https://mls.kuu.la/share/collection/7FCcS?fs=1&vr=1&zoom=1&autorotate=0.04&autop=100&autopalt=1&thumbs=3&alpha=0.82"
-                width="100%"
-                height="400" // İframe yüksekliğini istediğiniz gibi ayarlayabilirsiniz
-                frameBorder="0"
-                allowFullScreen
-              ></iframe>
-              
-            </Link>
-          </Card>
+        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-3 ">
+		{Object.keys(projects).map((key, index) => {
+            const project = projects[key]; // project'ı tanımlayın
+            return (
+              <div key={index} className="grid grid-cols-1 gap-4">
+                <Card>
+                  <Link href={`/projects`}>
+                    <iframe
+                      src={project.link}
+                      width={project.width}
+                      height={project.height}
+                      frameBorder="0"
+                      allowFullScreen={project.fullscreen}
+					  
+                    ></iframe>
+                  </Link>
+                </Card>
+              </div>
+            );
+          })}
 
           <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 "></div>
         </div>
---
+++ util/dataTypes.ts
@@ -0,0 +1,8 @@
+export type Data = {
+    [key: string]: {
+      link: string;
+      width: string;
+      height: string;
+      fullscreen: boolean;
+    };
+  };
